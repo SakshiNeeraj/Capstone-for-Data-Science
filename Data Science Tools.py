9625dc5b333fedbe2ece2b581fe6864a7978745c
Sakshi Neeraj <75196984+SakshiNeeraj@users.noreply.github.com>
2023-05-15 12:11:45 +0530

    Add files via upload

---
+++ Data Science Tools.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# DATA SCIENCE TOOLS
+
+# This notebook has brief about the tools used and libraries used for Data Science Operations
+
+# 1. Python
+# 2. R
+# 3. SQL
+# 4. Julia
+# 5. Scala
+# 6. Matlab
+
+# 1. Pandas
+# 2. Numpy
+# 3. SkLearn
+# 4. Tensor Flow
+# 5. Nltk
+
+# 1. Jupyter Notebook
+# 2. RStudio
+# 3. Apache Spark
+# 4. TensorFlow
+# 5. PyTorch
+# 6. Tableau
+# 7. SQL Database
+# 8. Hadoop
+# 9. IBM Watson Studio
+# 10. Git and Github
+
+# 1. ADD -> 5+6
+# 2. SUBTRACT -> 5-6
+# 3. Multiply -> 5*6
+# 4. Division -> 5/6
+# 5. Power -> 5**6
+# 6. Floor Division -> 5//6
+
+# In[1]:
+
+
+a=5
+b=6
+print("a+b=",a+b) # ADD
+print("a*b=",a*b) #SUBTRACT
+
+
+# In[2]:
+
+
+minutes=120
+print("Hours=",minutes/60)
+
+
+# OBJECTIVES
+# 
+# Exercise 2 - Create a markdown cell with the title of the notebook. (1 pt)
+# 
+# Exercise 3 - Create a markdown cell for an introduction. (1 pt)
+# 
+# Exercise 4 - Create a markdown cell to list data science languages. (3 pts)
+# 
+# Exercise 5 - Create a markdown cell to list data science libraries. (3 pts)
+# 
+# Exercise 6 - Create a markdown cell with a table of Data Science tools. (3 pts)
+# 
+# Exercise 7 - Create a markdown cell introducing arithmetic expression examples. (1 pt)
+# 
+# Exercise 8 - Create a code cell to multiply and add numbers. (2 pts)
+# 
+# Exercise 9 - Create a code cell to convert minutes to hours. (2 pts)
+# 
+# Exercise 10 -Insert a markdown cell to list Objectives. (3 pts)
+# 
+# Exercise 11 - Create a markdown cell to indicate the Author’s name. (2 pts)
+# 
+# Exercise 12 - Share your notebook through GitHub (3 pts)
+# 
+# Exercise 13 - Take a screenshot of the first page of the notebook. (1 pt)
+
+# Author Name- Sakshi Neeraj
+
+# In[ ]:
+
+
+
+
